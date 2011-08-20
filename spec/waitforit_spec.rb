adf6820589d88217478f2e9154deffe3d9c13f49
Leon Davis <leon_a_d@yahoo.com>
2011-08-20 23:46:46 +0100

    allow user to specifiy how long they are prepared to wait

--- spec/waitforit_spec.rb
+++ spec/waitforit_spec.rb
@@ -13,4 +13,13 @@ describe 'waitforit' do
   it 'should throw an exception if when the prescribed action does not happen in time' do
     expect{wait_until(0.5){false}}.to raise_error RuntimeError
   end
+  
+  it 'should keep trying for a specified period' do
+    start_time = Time.now
+    prescribed_wait_time = 1
+    
+    lambda{wait_until(prescribed_wait_time){false}}
+    
+    (start_time + prescribed_wait_time).should be >= Time.now 
+  end
 end
