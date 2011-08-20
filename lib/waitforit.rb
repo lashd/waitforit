7ad45676209764b86ce8c73c75578c78aa21a60e
Leon Davis <leon_a_d@yahoo.com>
2011-08-20 23:47:26 +0100

    allow user to specifiy how long they are prepared to wait

--- lib/waitforit.rb
+++ lib/waitforit.rb
@@ -1,7 +1,8 @@
 def wait_until max_wait_time=10
   start_time = Time.now
   until Time.now > start_time + max_wait_time
-    return if yield == true 
+    return if yield == true
+    sleep 0.5
   end
   raise RuntimeError, "Action took to long"
 end
