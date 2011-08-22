8c7c5eac78c1ebc1067c689fe261fb4109bdebc7
Leon Davis <leon_a_d@yahoo.com>
2011-08-22 23:25:57 +0100

    adding methods for minutes, hours and days

--- lib/waitforit.rb
+++ lib/waitforit.rb
@@ -2,7 +2,23 @@ module TimeIncrements
   def seconds
     self * 1
   end
+  
+  def minutes
+    seconds * 60
+  end
+  
+  def hours
+    minutes * 60
+  end
+  
+  def days
+    hours * 24
+  end
+  
   alias_method :second, :seconds
+  alias_method :minute, :minutes
+  alias_method :hour, :hours
+  alias_method :day, :days
 end
 class Float
   include TimeIncrements
